6810abea3a039f372fd79ba5ccf562f96e3955e8
Kyle Wiley <chansolle@gmail.com>
2022-06-02 07:36:28 +0700

    fixed movie detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
 import "./App.css";
 import './grid.css';
 import Home from "components/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Detail from "components/Detail";
+// import Detail from "components/Detail";
 import About from "components/About";
+const Detail = React.lazy(() => import('components/Detail'))
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/home' element={<Home />} />
-        <Route path='/detail/:category/:id' element={<Detail />} />
+        <Route path='/detail/:category/:id' element={
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Detail />
+          </React.Suspense>
+        } />
         <Route path='/about' element={<About />} />
         <Route path='/' element={<Navigate to='/home' />} />
       </Routes>
--- src/components/MovieDetail/index.js
+++ src/components/MovieDetail/index.js
@@ -1,7 +1,7 @@
 import tmdbApi from 'api/tmdbApi';
 import SimilarItem from 'components/SimilarItem';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './movie.css'
 
 function Movie(props) {
@@ -29,124 +29,126 @@ function Movie(props) {
 
     getMovieDetail()
   }, [category, id])
+  const episodesTv = Array.from({ length: detail?.next_episode_to_air?.episode_number || detail?.number_of_episodes }, (_, i) => i + 1)
   console.log(detail);
-  const episodes = Array.from({ length: detail?.next_episode_to_air?.episode_number || detail?.number_of_episodes }, (_, i) => i + 1)
-  console.log(episodes);
 
   return (
     <div className="section">
-      <div className="container">
-        <div className="row">
-          <div className="col-8 col-md-12 col-sm-12">
-            <div className="video-container">
-              {/* <video controls>
-                <source src="https://viettrung258.work/film" />
-                Video is not supported!
-              </video> */}
-              <iframe title='video-detail' width="420" height="315"
-                src={`https://www.youtube-nocookie.com/embed/${movie.key}`}>
-              </iframe>
-            </div>
-
-            <div className="movie-details">
-              <h1>{detail.title || detail.original_name}</h1>
-              <div className="movie-info">
-                <div className="rating">
-                  <span className="star">
-                    <i className='bx bxs-star'></i>
-                  </span>
-                  <span className="score">{detail.vote_average}</span>
-                </div>
-                <div className="release">
-                  <span className="calendar">
-                    <i className='bx bxs-calendar'></i>
-                  </span>
-                  <span className="year">{detail.release_date}</span>
-                </div>
+      {movie ?
+        <div className="container">
+          <div className="row">
+            <div className="col-8 col-md-12 col-sm-12">
+              <div className="video-container">
+                {/* <video controls>
+            <source src="https://viettrung258.work/film" />
+            Video is not supported!
+          </video> */}
+                <iframe title='video-detail' width="420" height="315"
+                  src={`https://www.youtube-nocookie.com/embed/${movie.key}`}>
+                </iframe>
               </div>
-              <div className="movie-genre">
-                {genres.map(genre => (
-                  <a href='=' className="genre" key={genre.id}>{genre.name}</a>
-                ))}
-              </div>
-              <div className="movie-description">
-                <p>
-                  {detail.overview}
-                </p>
-              </div>
-            </div>
-            {
-              category === 'tv' &&
-              <>
-                <h2>Episodes</h2>
-                <div className="episodes-list">
-                  <a href="2" className="episode active">1</a>
-                  <a href="3" className="episode">2</a>
-                  <a href="4" className="episode">3</a>
-                </div>
-              </>
-            }
 
-          </div>
-          <div className="col-4 col-md-12 col-sm-12">
-            <div className="similar-container">
-              <div className="header">
-                <h2>Similar to this</h2>
-              </div>
-              <div className="similar-list">
-                {similarMovies.map(similarMovie => (
-                  <SimilarItem similar={similarMovie} key={similarMovie.id} />
-                ))}
-              </div>
-            </div>
-          </div>
-          {/* Comment section */}
-          <div className="col-8 col-md-12 col-sm-12">
-            <div className="comments-section">
-              <h2>Comments</h2>
-
-              {/* <!-- <div className="comment-alert">
-              <div className="avatar">
-                <img src="https://filmhot.live/default-avatar.png" alt="">
-              </div>
-              <p>You need to <a>Sign in</a> to comment !</p>
-            </div> --> */}
-
-              <form className="comment-box">
-                <div className="avatar">
-                  <img src="https://filmhot.live/default-avatar.png" alt="" />
-                </div>
-                <input type="text" placeholder="Comment what yout think..." />
-                <button>
-                  <i className='bx bxs-send'></i>
-                </button>
-              </form>
-
-              {/* <h4 style="text-align:center">No one has commented </h4>  */}
-
-              <div className="comment-item">
-                <img
-                  src="https://images.weserv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14GhUigrReywgtRTgeW77KiP1up9ro5ruHpIla3bmnQ%3Ds96-c&w=50&h=50&fit=outside"
-                  alt="" />
-                <div className="info">
-                  <div className="info-name">
-                    <span className="name">Én Én</span>
-                    <span className="time">Just now</span>
+              <div className="movie-details">
+                <h1>{detail.title || detail.original_name}</h1>
+                <div className="movie-info">
+                  <div className="rating">
+                    <span className="star">
+                      <i className='bx bxs-star'></i>
+                    </span>
+                    <span className="score">{detail.vote_average}</span>
                   </div>
-                  <div className="content">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, esse tempore deleniti dolore
-                    laborum obcaecati assumenda quae eaque iusto quod quo quasi sed. Dicta itaque numquam libero aperiam
-                    tempora beatae.
-                    Doloribus veritatis ipsam itaque nesciunt ea inventore provident exercitationem consectetur totam.
-                    Natus, neque perspiciatis. Sequi asperiores perferendis possimus mollitia! Numquam doloremque ipsa ea
-                    aliquid dolorum nulla excepturi quod totam illo.
+                  <div className="release">
+                    <span className="calendar">
+                      <i className='bx bxs-calendar'></i>
+                    </span>
+                    <span className="year">{detail.release_date}</span>
+                  </div>
+                </div>
+                <div className="movie-genre">
+                  {genres.map(genre => (
+                    <a href='=' className="genre" key={genre.id}>{genre.name}</a>
+                  ))}
+                </div>
+                <div className="movie-description">
+                  <p>
+                    {detail.overview}
+                  </p>
+                </div>
+              </div>
+              {
+                category === 'tv' &&
+                <>
+                  <h2>Episodes</h2>
+                  <div className="episodes-list">
+                    {episodesTv.map(episode => (
+                      <Link to={{ pathname: `?episode=${episode}` }} activeClassName='active' className="episode" key={episode}>{episode}</Link>
+                    ))}
+                  </div>
+                </>
+              }
+
+            </div>
+            <div className="col-4 col-md-12 col-sm-12">
+              <div className="similar-container">
+                <div className="header">
+                  <h2>Similar to this</h2>
+                </div>
+                <div className="similar-list">
+                  {similarMovies.map(similarMovie => (
+                    <SimilarItem similar={similarMovie} key={similarMovie.id} />
+                  ))}
+                </div>
+              </div>
+            </div>
+            {/* Comment section */}
+            <div className="col-8 col-md-12 col-sm-12">
+              <div className="comments-section">
+                <h2>Comments</h2>
+
+                {/* <!-- <div className="comment-alert">
+          <div className="avatar">
+            <img src="https://filmhot.live/default-avatar.png" alt="">
+          </div>
+          <p>You need to <a>Sign in</a> to comment !</p>
+        </div> --> */}
+
+                <form className="comment-box">
+                  <div className="avatar">
+                    <img src="https://filmhot.live/default-avatar.png" alt="" />
+                  </div>
+                  <input type="text" placeholder="Comment what yout think..." />
+                  <button>
+                    <i className='bx bxs-send'></i>
+                  </button>
+                </form>
+
+                {/* <h4 style="text-align:center">No one has commented </h4>  */}
+
+                <div className="comment-item">
+                  <img
+                    src="https://images.weserv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14GhUigrReywgtRTgeW77KiP1up9ro5ruHpIla3bmnQ%3Ds96-c&w=50&h=50&fit=outside"
+                    alt="" />
+                  <div className="info">
+                    <div className="info-name">
+                      <span className="name">Én Én</span>
+                      <span className="time">Just now</span>
+                    </div>
+                    <div className="content">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, esse tempore deleniti dolore
+                      laborum obcaecati assumenda quae eaque iusto quod quo quasi sed. Dicta itaque numquam libero aperiam
+                      tempora beatae.
+                      Doloribus veritatis ipsam itaque nesciunt ea inventore provident exercitationem consectetur totam.
+                      Natus, neque perspiciatis. Sequi asperiores perferendis possimus mollitia! Numquam doloremque ipsa ea
+                      aliquid dolorum nulla excepturi quod totam illo.
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        : <h1 style={{ textAlign: 'center' }}>Data not found!</h1>
+      }
     </div>
   );
 }
