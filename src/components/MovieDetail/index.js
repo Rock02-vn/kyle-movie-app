0739be8c176fae2a43c2aa5c34546ff7b08c02be
Kyle Wiley <chansolle@gmail.com>
2022-06-04 15:45:10 +0700

    post comment

--- src/components/MovieDetail/index.js
+++ src/components/MovieDetail/index.js
@@ -1,43 +1,96 @@
-import tmdbApi from 'api/tmdbApi';
-import SimilarItem from 'components/SimilarItem';
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import './movie.css'
+import tmdbApi from "api/tmdbApi";
+import axios from "axios";
+import SimilarItem from "components/SimilarItem";
+import useDebounce from "hooks/useDebounce";
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import "./movie.css";
 
 function Movie(props) {
   const { category, id } = useParams();
-  const [movie, setMovie] = useState({})
-  const [detail, setDetail] = useState({})
-  const [genres, setGenres] = useState([])
-  const [similarMovies, setSimilarMovies] = useState([])
+  const [movie, setMovie] = useState({});
+  const [detail, setDetail] = useState({});
+  const [genres, setGenres] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [commentContent, setCommentContent] = useState("");
 
   const isUser = useSelector((state) => state.auth.currentUser);
+  const commentBaseUrl = "https://json-api-public.herokuapp.com/api/comments";
+  const debounced = useDebounce(commentContent, 300);
 
+  const episodesTv = Array.from(
+    {
+      length:
+        detail?.next_episode_to_air?.episode_number ||
+        detail?.number_of_episodes,
+    },
+    (_, i) => i + 1
+  );
+
+  // Get detail movie
   useEffect(() => {
     const getMovieDetail = async () => {
-      const response = await tmdbApi.getVideos(category, id)
-      const response2 = await tmdbApi.similar(category, id)
-      const response3 = await tmdbApi.detail(category, id, { params: {} })
-      setMovie(response.results[response.results.length - 1])
-      setSimilarMovies(response2.results)
-      setDetail(response3)
-      setGenres(response3.genres)
-    }
+      const response = await tmdbApi.getVideos(category, id);
+      const response2 = await tmdbApi.similar(category, id);
+      const response3 = await tmdbApi.detail(category, id, { params: {} });
+      setMovie(response.results[response.results.length - 1]);
+      setSimilarMovies(response2.results);
+      setDetail(response3);
+      setGenres(response3.genres);
+    };
 
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
 
-    getMovieDetail()
-  }, [category, id])
-  const episodesTv = Array.from({ length: detail?.next_episode_to_air?.episode_number || detail?.number_of_episodes }, (_, i) => i + 1)
+    getMovieDetail();
+  }, [category, id]);
+
+  const handlePostComment = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const data = {
+        displayName: isUser.displayName,
+        photoUrl: isUser.photoURL,
+        uid: isUser.uid,
+        commentContent: debounced,
+        movieId: detail.id,
+      };
+      await axios.post(`${commentBaseUrl}`, data);
+      setCommentContent("");
+    },
+    [debounced, detail.id]
+  );
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const res = await axios.get(`${commentBaseUrl}?movieId=${detail.id}`);
+      setComments(res.data);
+    };
+
+    fetchComments();
+  }, [detail.id, handlePostComment]);
+
+  useEffect(() => {
+    const enterEvent = (e) => {
+      if (e.keyCode === 13) handlePostComment();
+    };
+
+    document.addEventListener("keyup", enterEvent);
+    return () => {
+      document.removeEventListener("keyup", enterEvent);
+    };
+  }, [handlePostComment]);
+
+  console.log(comments);
 
   return (
     <div className="section">
-      {movie ?
+      {movie ? (
         <div className="container">
           <div className="row">
             <div className="col-8 col-md-12 col-sm-12">
@@ -46,9 +99,12 @@ function Movie(props) {
             <source src="https://viettrung258.work/film" />
             Video is not supported!
           </video> */}
-                <iframe title='video-detail' width="420" height="315"
-                  src={`https://www.youtube-nocookie.com/embed/${movie.key}`}>
-                </iframe>
+                <iframe
+                  title="video-detail"
+                  width="420"
+                  height="315"
+                  src={`https://www.youtube-nocookie.com/embed/${movie.key}`}
+                ></iframe>
               </div>
 
               <div className="movie-details">
@@ -56,40 +112,47 @@ function Movie(props) {
                 <div className="movie-info">
                   <div className="rating">
                     <span className="star">
-                      <i className='bx bxs-star'></i>
+                      <i className="bx bxs-star"></i>
                     </span>
                     <span className="score">{detail.vote_average}</span>
                   </div>
                   <div className="release">
                     <span className="calendar">
-                      <i className='bx bxs-calendar'></i>
+                      <i className="bx bxs-calendar"></i>
                     </span>
-                    <span className="year">{detail.release_date || detail.first_air_date}</span>
+                    <span className="year">
+                      {detail.release_date || detail.first_air_date}
+                    </span>
                   </div>
                 </div>
                 <div className="movie-genre">
-                  {genres.map(genre => (
-                    <a href='=' className="genre" key={genre.id}>{genre.name}</a>
+                  {genres.map((genre) => (
+                    <a href="=" className="genre" key={genre.id}>
+                      {genre.name}
+                    </a>
                   ))}
                 </div>
                 <div className="movie-description">
-                  <p>
-                    {detail.overview}
-                  </p>
+                  <p>{detail.overview}</p>
                 </div>
               </div>
-              {
-                category === 'tv' &&
+              {category === "tv" && (
                 <>
                   <h2>Episodes</h2>
                   <div className="episodes-list">
-                    {episodesTv.map(episode => (
-                      <Link to={{ pathname: `?episode=${episode}` }} activeclassname='active' className="episode" key={episode}>{episode}</Link>
+                    {episodesTv.map((episode) => (
+                      <Link
+                        to={{ pathname: `?episode=${episode}` }}
+                        activeclassname="active"
+                        className="episode"
+                        key={episode}
+                      >
+                        {episode}
+                      </Link>
                     ))}
                   </div>
                 </>
-              }
-
+              )}
             </div>
             <div className="col-4 col-md-12 col-sm-12">
               <div className="similar-container">
@@ -97,7 +160,7 @@ function Movie(props) {
                   <h2>Similar to this</h2>
                 </div>
                 <div className="similar-list">
-                  {similarMovies.map(similarMovie => (
+                  {similarMovies.map((similarMovie) => (
                     <SimilarItem similar={similarMovie} key={similarMovie.id} />
                   ))}
                 </div>
@@ -109,63 +172,74 @@ function Movie(props) {
                 <h2>Comments</h2>
 
                 {/* When user logged in */}
-                {isUser &&
+                {isUser && (
                   <form className="comment-box">
                     <div className="avatar">
                       <img
                         style={{
                           width: "30px",
-                          borderRadius: "50%"
+                          borderRadius: "50%",
                         }}
-                        src={isUser.photoURL} alt="" />
+                        src={isUser.photoURL}
+                        alt={isUser.photoURL}
+                      />
                     </div>
-                    <input type="text" placeholder="Comment what yout think..." />
-                    <button>
-                      <i className='bx bxs-send'></i>
+                    <input
+                      type="text"
+                      placeholder="Comment what yout think..."
+                      value={commentContent}
+                      onChange={(e) => setCommentContent(e.target.value)}
+                    />
+                    <button onClick={(e) => handlePostComment(e)}>
+                      <i className="bx bxs-send"></i>
                     </button>
                   </form>
-                }
+                )}
 
                 {/* When user not logged in*/}
-                {!isUser &&
+                {!isUser && (
                   <div className="comment-alert">
                     <div className="avatar">
-                      <img style={{
-                        width: "30px",
-                        borderRadius: "50%"
-                      }} src="https://filmhot.live/default-avatar.png" alt="" />
+                      <img
+                        style={{
+                          width: "30px",
+                          borderRadius: "50%",
+                        }}
+                        src="https://filmhot.live/default-avatar.png"
+                        alt=""
+                      />
                     </div>
-                    <p>You need to <Link to='/login'>Sign in</Link> to comment !</p>
+                    <p>
+                      You need to <Link to="/login">Sign in</Link> to comment !
+                    </p>
                   </div>
-                }
+                )}
 
-                {/* <h4 style="text-align:center">No one has commented </h4>  */}
+                {comments.map((user) => {
+                  return (
+                    <div className="comment-item" key={user.id}>
+                      <img src={user.photoUrl} alt="" />
+                      <div className="info">
+                        <div className="info-name">
+                          <span className="name">{user.displayName}</span>
+                          <span className="time">{user.createdAt}</span>
+                        </div>
+                        <div className="content">{user.commentContent}</div>
+                      </div>
+                    </div>
+                  );
+                })}
 
-                <div className="comment-item">
-                  <img
-                    src="https://images.weserv.nl/?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa-%2FAOh14GhUigrReywgtRTgeW77KiP1up9ro5ruHpIla3bmnQ%3Ds96-c&w=50&h=50&fit=outside"
-                    alt="" />
-                  <div className="info">
-                    <div className="info-name">
-                      <span className="name">Én Én</span>
-                      <span className="time">Just now</span>
-                    </div>
-                    <div className="content">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, esse tempore deleniti dolore
-                      laborum obcaecati assumenda quae eaque iusto quod quo quasi sed. Dicta itaque numquam libero aperiam
-                      tempora beatae.
-                      Doloribus veritatis ipsam itaque nesciunt ea inventore provident exercitationem consectetur totam.
-                      Natus, neque perspiciatis. Sequi asperiores perferendis possimus mollitia! Numquam doloremque ipsa ea
-                      aliquid dolorum nulla excepturi quod totam illo.
-                    </div>
-                  </div>
-                </div>
+                {comments.length === 0 && (
+                  <h4 style={{ textAlign: "center" }}>No one has commented</h4>
+                )}
               </div>
             </div>
           </div>
         </div>
-        : <h1 style={{ textAlign: 'center' }}>Data not found!</h1>
-      }
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Data not found!</h1>
+      )}
     </div>
   );
 }
